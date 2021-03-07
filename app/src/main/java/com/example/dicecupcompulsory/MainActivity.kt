f34c041b46b4bd671b0cee17fbcdfb4549836edb
ArmNem <58069062+ArmNem@users.noreply.github.com>
2021-03-07 14:04:30 +0100

    history, clear,back,listview,landscape

--- app/src/main/java/com/example/dicecupcompulsory/MainActivity.kt
+++ app/src/main/java/com/example/dicecupcompulsory/MainActivity.kt
@@ -1,43 +1,38 @@
 package com.example.dicecupcompulsory
 
-import android.nfc.Tag
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
-import androidx.core.view.get
-import androidx.core.view.isInvisible
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
-import java.sql.Time
-import java.sql.Timestamp
-import java.time.LocalDateTime
 import java.time.LocalDateTime.now
 import java.time.format.DateTimeFormatter
 import java.util.*
-import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
 
-    private val diceId = intArrayOf(0, R.drawable.dice1,
+    private val diceId = intArrayOf(
+            0, R.drawable.dice1,
             R.drawable.dice2,
             R.drawable.dice3,
             R.drawable.dice4,
             R.drawable.dice5,
-            R.drawable.dice6)
-    private val HISTORY_NAME = "history"
-    private val mHistory = HashMap<String,List<Int>>()
+            R.drawable.dice6
+    )
+    private val mHistory = mutableListOf<Pair<String, List<Int>>>()
 
     private val mRandomGenerator = Random()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         BtnRoll.setOnClickListener { v -> onClickRoll() }
+        BtnHistory.setOnClickListener { v -> onClickHistory() }
         populateSpinner()
     }
+
     private fun onClickRoll() {
         val list = mutableListOf<Int>()
         val current = now()
@@ -45,33 +40,29 @@ class MainActivity : AppCompatActivity() {
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
         val formatted = current.format(formatter)
 
-        if (SpDices.selectedItemPosition == 0)
-        {
-        val e1 = mRandomGenerator.nextInt(6) + 1
+        if (SpDices.selectedItemPosition == 0) {
+            val e1 = mRandomGenerator.nextInt(6) + 1
             list.add(e1)
-                mHistory.put(formatted,list)
+            mHistory.add(Pair(formatted, list))
 
         }
-        if (SpDices.selectedItemPosition == 1)
-        {
+        if (SpDices.selectedItemPosition == 1) {
             val e1 = mRandomGenerator.nextInt(6) + 1
             val e2 = mRandomGenerator.nextInt(6) + 1
             list.add(e1)
             list.add(e2)
-            mHistory.put(formatted,list)
+            mHistory.add(Pair(formatted, list))
         }
-        if (SpDices.selectedItemPosition == 2)
-        {
+        if (SpDices.selectedItemPosition == 2) {
             val e1 = mRandomGenerator.nextInt(6) + 1
             val e2 = mRandomGenerator.nextInt(6) + 1
             val e3 = mRandomGenerator.nextInt(6) + 1
             list.add(e1)
             list.add(e2)
             list.add(e3)
-            mHistory.put(formatted,list)
+            mHistory.add(Pair(formatted, list))
         }
-        if (SpDices.selectedItemPosition == 3)
-        {
+        if (SpDices.selectedItemPosition == 3) {
             val e1 = mRandomGenerator.nextInt(6) + 1
             val e2 = mRandomGenerator.nextInt(6) + 1
             val e3 = mRandomGenerator.nextInt(6) + 1
@@ -80,10 +71,9 @@ class MainActivity : AppCompatActivity() {
             list.add(e2)
             list.add(e3)
             list.add(e4)
-            mHistory.put(formatted,list)
+            mHistory.add(Pair(formatted, list))
         }
-        if (SpDices.selectedItemPosition == 4)
-        {
+        if (SpDices.selectedItemPosition == 4) {
             val e1 = mRandomGenerator.nextInt(6) + 1
             val e2 = mRandomGenerator.nextInt(6) + 1
             val e3 = mRandomGenerator.nextInt(6) + 1
@@ -94,10 +84,9 @@ class MainActivity : AppCompatActivity() {
             list.add(e3)
             list.add(e4)
             list.add(e5)
-            mHistory.put(formatted,list)
+            mHistory.add(Pair(formatted, list))
         }
-        if (SpDices.selectedItemPosition == 5)
-        {
+        if (SpDices.selectedItemPosition == 5) {
             val e1 = mRandomGenerator.nextInt(6) + 1
             val e2 = mRandomGenerator.nextInt(6) + 1
             val e3 = mRandomGenerator.nextInt(6) + 1
@@ -110,12 +99,18 @@ class MainActivity : AppCompatActivity() {
             list.add(e4)
             list.add(e5)
             list.add(e6)
-            mHistory.put(formatted,list)
+            mHistory.add(Pair(formatted, list))
         }
         updateDicesWith(list)
-
-        updateHistory()
+        // updateHistory()
     }
+
+    private fun onClickHistory() {
+        val i = Intent(this, HistoryActivity::class.java)
+        i.putExtra("history", mHistory.toTypedArray())
+        startActivityForResult(i, 5)
+    }
+
     private fun updateDicesWith(list: List<Int>) {
         if (list.size == 1) {
             imgDice1.setImageResource(diceId[list.first()])
@@ -152,76 +147,102 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-    private fun populateSpinner(){
+
+    private fun populateSpinner() {
         ArrayAdapter.createFromResource(
-            this,
-            R.array.dices,
-            android.R.layout.simple_spinner_item
+                this,
+                R.array.dices,
+                android.R.layout.simple_spinner_item
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             SpDices.adapter = adapter
 
-            SpDices.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            SpDices.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
                 override fun onNothingSelected(parent: AdapterView<*>?) {}
                 override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    position: Int,
-                    id: Long
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
                 ) {
-                            when (position) {
-                                0 -> {
-                                imgDice2.isVisible = false
-                                    imgDice3.isVisible = false
-                                    imgDice4.isVisible = false
-                                    imgDice5.isVisible = false
-                                    imgDice6.isVisible = false
-                                }
-                                1 -> {
-                                    imgDice2.isVisible = true
-                                    imgDice3.isVisible = false
-                                    imgDice4.isVisible = false
-                                    imgDice5.isVisible = false
-                                    imgDice6.isVisible = false
-                                }
-                                2 -> {
-                                    imgDice2.isVisible = true
-                                    imgDice3.isVisible = true
-                                    imgDice4.isVisible = false
-                                    imgDice5.isVisible = false
-                                    imgDice6.isVisible = false
-                                }
-                                3 -> {
-                                    imgDice2.isVisible = true
-                                    imgDice3.isVisible = true
-                                    imgDice4.isVisible = true
-                                    imgDice5.isVisible = false
-                                    imgDice6.isVisible = false
-                                }
-                                4 -> {
-                                    imgDice2.isVisible = true
-                                    imgDice3.isVisible = true
-                                    imgDice4.isVisible = true
-                                    imgDice5.isVisible = true
-                                    imgDice6.isVisible = false
-                                }
-                                5 -> {
-                                    imgDice2.isVisible = true
-                                    imgDice3.isVisible = true
-                                    imgDice4.isVisible = true
-                                    imgDice5.isVisible = true
-                                    imgDice6.isVisible = true
-                                }
-                            }
+                    when (position) {
+                        0 -> {
+                            imgDice2.isVisible = false
+                            imgDice3.isVisible = false
+                            imgDice4.isVisible = false
+                            imgDice5.isVisible = false
+                            imgDice6.isVisible = false
+                        }
+                        1 -> {
+                            imgDice2.isVisible = true
+                            imgDice3.isVisible = false
+                            imgDice4.isVisible = false
+                            imgDice5.isVisible = false
+                            imgDice6.isVisible = false
+                        }
+                        2 -> {
+                            imgDice2.isVisible = true
+                            imgDice3.isVisible = true
+                            imgDice4.isVisible = false
+                            imgDice5.isVisible = false
+                            imgDice6.isVisible = false
+                        }
+                        3 -> {
+                            imgDice2.isVisible = true
+                            imgDice3.isVisible = true
+                            imgDice4.isVisible = true
+                            imgDice5.isVisible = false
+                            imgDice6.isVisible = false
+                        }
+                        4 -> {
+                            imgDice2.isVisible = true
+                            imgDice3.isVisible = true
+                            imgDice4.isVisible = true
+                            imgDice5.isVisible = true
+                            imgDice6.isVisible = false
+                        }
+                        5 -> {
+                            imgDice2.isVisible = true
+                            imgDice3.isVisible = true
+                            imgDice4.isVisible = true
+                            imgDice5.isVisible = true
+                            imgDice6.isVisible = true
+                        }
+                    }
 
                 }
             }
-        }}
+        }
+    }
+
     private fun updateHistory() {
-        val result = mHistory.toList().sortedBy { (value, _) -> value}.toMap()
         var s = ""
-        result.forEach { p ->  val e1 = p.key; val e2 = p.value;  s += "$e1 - $e2 \n" }
+        mHistory.forEach { p ->
+            val e1 = p.first;
+            val e2 = p.second; s += "$e1 - $e2 \n"
+        }
         tvHistory.text = s
     }
 
+    fun clearHistory(view: View) {
+        mHistory.clear()
+        updateHistory()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 5) {
+            if (resultCode == 10)
+                mHistory.clear()
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable("history", mHistory.toTypedArray())
+    }
 }
+
+
+
+
+
